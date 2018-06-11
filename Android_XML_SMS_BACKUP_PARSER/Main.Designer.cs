88c8022786ff1b1441d5c32a942d154753c0eb06
aryampa <aryampa2caleb@yahoo.com>
2018-06-11 19:15:19 +1000

    New File

---
+++ Android_XML_SMS_BACKUP_PARSER/Main.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Android_XML_SMS_BACKUP_PARSER
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxPrevXml = new System.Windows.Forms.TextBox();
+            this.btnLoadXML = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxParsePrev = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.rbtnText = new System.Windows.Forms.RadioButton();
+            this.rbtnExcel = new System.Windows.Forms.RadioButton();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbxPrevXml
+            // 
+            this.tbxPrevXml.Location = new System.Drawing.Point(13, 62);
+            this.tbxPrevXml.Multiline = true;
+            this.tbxPrevXml.Name = "tbxPrevXml";
+            this.tbxPrevXml.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxPrevXml.Size = new System.Drawing.Size(262, 252);
+            this.tbxPrevXml.TabIndex = 0;
+            // 
+            // btnLoadXML
+            // 
+            this.btnLoadXML.Location = new System.Drawing.Point(13, 5);
+            this.btnLoadXML.Name = "btnLoadXML";
+            this.btnLoadXML.Size = new System.Drawing.Size(100, 35);
+            this.btnLoadXML.TabIndex = 1;
+            this.btnLoadXML.Text = "Load XML FILE";
+            this.btnLoadXML.UseVisualStyleBackColor = true;
+            this.btnLoadXML.Click += new System.EventHandler(this.btnLoadXML_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "XML Preview:";
+            // 
+            // tbxParsePrev
+            // 
+            this.tbxParsePrev.Location = new System.Drawing.Point(296, 62);
+            this.tbxParsePrev.Multiline = true;
+            this.tbxParsePrev.Name = "tbxParsePrev";
+            this.tbxParsePrev.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbxParsePrev.Size = new System.Drawing.Size(347, 252);
+            this.tbxParsePrev.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(293, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Parsing Results:";
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(524, 1);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(118, 35);
+            this.btnParse.TabIndex = 5;
+            this.btnParse.Text = "Parse XML";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // rbtnText
+            // 
+            this.rbtnText.AutoSize = true;
+            this.rbtnText.Location = new System.Drawing.Point(293, 362);
+            this.rbtnText.Name = "rbtnText";
+            this.rbtnText.Size = new System.Drawing.Size(71, 17);
+            this.rbtnText.TabIndex = 6;
+            this.rbtnText.TabStop = true;
+            this.rbtnText.Text = "Text/Doc";
+            this.rbtnText.UseVisualStyleBackColor = true;
+            // 
+            // rbtnExcel
+            // 
+            this.rbtnExcel.AutoSize = true;
+            this.rbtnExcel.Location = new System.Drawing.Point(293, 339);
+            this.rbtnExcel.Name = "rbtnExcel";
+            this.rbtnExcel.Size = new System.Drawing.Size(77, 17);
+            this.rbtnExcel.TabIndex = 7;
+            this.rbtnExcel.TabStop = true;
+            this.rbtnExcel.Text = "Excel/CSV";
+            this.rbtnExcel.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(524, 339);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(117, 35);
+            this.btnCreate.TabIndex = 8;
+            this.btnCreate.Text = "Create File";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lblStats
+            // 
+            this.lblStats.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblStats.ForeColor = System.Drawing.Color.Red;
+            this.lblStats.Location = new System.Drawing.Point(13, 321);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(274, 58);
+            this.lblStats.TabIndex = 9;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 382);
+            this.Controls.Add(this.lblStats);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.rbtnExcel);
+            this.Controls.Add(this.rbtnText);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxParsePrev);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoadXML);
+            this.Controls.Add(this.tbxPrevXml);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxPrevXml;
+        private System.Windows.Forms.Button btnLoadXML;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxParsePrev;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.RadioButton rbtnText;
+        private System.Windows.Forms.RadioButton rbtnExcel;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lblStats;
+    }
+}
+
---
+++ Android_XML_SMS_BACKUP_PARSER/Main.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Spire.Doc;
+using System.Xml;
+
+namespace Android_XML_SMS_BACKUP_PARSER
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        string textblock = "";
+        List<string> fileLines = new List<string> { };
+
+        struct smsStructure {
+            public string Date;
+            public string address;
+            public string contactName;
+            public string Sent_Recieved;
+            public string openStatus;
+            public string ServiceCenterNumber;
+            public string Message;
+        }
+
+        enum openStatus { 
+            Opened,
+            Not_Opened,
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnParse.Enabled = false;
+            tbxParsePrev.Enabled = false;
+            btnCreate.Enabled = true;
+            lblStats.Text = "";
+            
+        }
+
+        private void btnLoadXML_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openXmlFile = new OpenFileDialog();
+                openXmlFile.Filter = "XML File (*.XML)|*.xml";
+                openXmlFile.Title = "Open SMS XML FILE";
+                openXmlFile.ShowDialog();
+
+                if (openXmlFile.FileName != "")
+                {
+                    StringBuilder sb = new StringBuilder();
+                    StreamReader sReader = new StreamReader(openXmlFile.FileName, true);
+
+                    string fileLine = sReader.ReadLine();
+                    while (fileLine != null) {
+
+                        fileLine =  fileLine.TrimStart(new char[] {'\t' });
+                        fileLines.Add(fileLine);
+                        fileLine = sReader.ReadLine();
+                    }
+
+                    for (int i = 0; i < fileLines.Count; i++) { 
+                        sb .AppendLine(fileLines[i]);
+                    }
+
+
+                    tbxPrevXml .Text = sb.ToString();
+                   // sb.AppendLine("Total Bytes: " + sReader.CurrentEncoding.GetByteCount(readString.ToCharArray()));
+                    
+                    //textblock = tbxPrevXml.Text;
+
+                    if (fileLines.Count  > 0)
+                    {
+                        btnParse.Enabled = true;
+                    }
+                    else btnParse.Enabled = false;
+                }
+                else { throw new Exception("No File Selected!!!"); }
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show("Error Openning XML File: " + ex.Message.ToString());
+            }
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            List<smsStructure> smsStructList = new List<smsStructure>();
+
+            try
+            {
+
+                if (fileLines[0][0] == '<' & fileLines[0][fileLines[0].Length - 1] == '>')
+                {
+                    if (!fileLines[0] .Contains("?xml")) throw new Exception("Invalid XML formating: ?xml attribute not found in root tag!");
+
+                }
+                else throw new Exception("Invalid XML formating: Check root Tag");
+
+                for (int i = 2; i < fileLines.Count ; i++)
+                {
+                    string line2process = fileLines[i].TrimStart(new char[] {'<' });
+                    line2process = line2process.TrimEnd(new char[] {'>' });
+
+                    if(line2process.StartsWith("sms")){
+                        line2process = line2process.Remove(0, "sms ".Length);
+                    }
+                    else if (line2process.StartsWith("allsms")) {
+                        line2process = line2process.Remove(0, "allsms ".Length);
+                       // MessageBox.Show("allsms"); 
+                    }
+                    //line2process = line2process.StartsWith("sms")
+
+                    smsStructure smsItem = parseSMSbody(line2process);
+
+                    if (smsItem.address != null) {
+                        smsStructList.Add(smsItem);
+                    }
+
+                }
+
+                MessageBox.Show(smsStructList.Count.ToString());
+
+                StringBuilder sbFinal = new StringBuilder();
+
+                Dictionary<string, List<smsStructure>> sms_groupingDictionary = new Dictionary<string, List<smsStructure>> { };
+
+                foreach (smsStructure sms_in_list in smsStructList) {
+
+                    if (sms_groupingDictionary.Keys.Contains(sms_in_list.address))
+                    {
+
+                        sms_groupingDictionary[sms_in_list.address].Add(sms_in_list);
+
+                    }
+
+                    else {
+                        sms_groupingDictionary.Add(sms_in_list.address, new List<smsStructure> {sms_in_list});
+                    }
+
+
+                }
+
+                foreach (string keys in sms_groupingDictionary.Keys) {
+                    List<smsStructure> smsList = sms_groupingDictionary[keys];
+                    
+
+                    sbFinal.AppendLine("__________ Conversations With:  "+keys+" ["+smsList[0].contactName +"] ____________________");
+                    
+                    foreach (smsStructure smsFinalItem in smsList) {
+
+                        sbFinal.AppendLine("- - - - - - - - - - - - - - - - - - - - ");
+                        sbFinal.AppendLine("Date: " + smsFinalItem.Date);
+                        sbFinal.AppendLine("Type: " + smsFinalItem.Sent_Recieved);
+                        sbFinal.AppendLine("Service Center Number: " + smsFinalItem.ServiceCenterNumber);
+                        sbFinal.AppendLine("Message:.---- ");
+                        sbFinal.AppendLine(smsFinalItem.Message);
+                    }
+
+                    sbFinal.AppendLine("");
+                }
+
+               
+                if (smsStructList.Count > 0) tbxParsePrev.Enabled = true;
+                else tbxParsePrev.Enabled = false;
+
+                tbxParsePrev.Text = sbFinal.ToString();
+
+       
+            }
+
+            catch (Exception ex) {
+                MessageBox.Show("Error Parsing XML String: " + ex.Message.ToString ());
+            }
+
+
+            
+
+
+        }
+
+        private string HumanReadable(string time) {
+
+            double   millssss = double .Parse(time);
+
+            DateTime startTimes = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            TimeSpan timedd = TimeSpan.FromMilliseconds(millssss);
+
+           return startTimes.AddMilliseconds(millssss ).ToLongDateString(); 
+        }
+
+        private smsStructure parseSMSbody(string rawSMS_string)
+        {
+            smsStructure smsObj = new smsStructure();
+            int quoteCounter = 0;
+            bool OpenQuoteSeen = false ;
+            bool CloseQuoteseen = false ;
+            string dataString = "";
+            for (int j = 0; j < rawSMS_string.Length; j++) {
+
+                char currentChar = rawSMS_string[j];
+                if (rawSMS_string[j] == '"' & quoteCounter == 0) { OpenQuoteSeen = true; }
+                if (rawSMS_string[j] == '"' & quoteCounter == 1) CloseQuoteseen = true;
+
+                if (OpenQuoteSeen & quoteCounter == 0) quoteCounter++;
+
+                if (OpenQuoteSeen & CloseQuoteseen)
+                {
+                    dataString = dataString + rawSMS_string[j].ToString();
+                    string[] attr_Val_array = dataString.Split(new char[] { '=' });
+                    if (attr_Val_array.Length != 2) { 
+
+                        if(attr_Val_array .Length <2){
+                            throw new Exception("Invalid attribute-value pair on string [" + dataString + "] in  line: " + rawSMS_string);
+                        }
+
+                        if (attr_Val_array.Length > 2) {
+                            string temp_str  = "";
+                            for (int k = 1; k < attr_Val_array.Length; k++) {
+                                temp_str = temp_str + attr_Val_array[k];
+                            }
+
+                            attr_Val_array[1] = temp_str;
+                        }
+
+                        
+                    }
+                    else
+                    {
+                        
+                        if (attr_Val_array[0].Trim().ToLower() == "date") {
+                            string newData = attr_Val_array[1].TrimStart(new char[] {'"' });
+                            newData = newData.TrimEnd(new char[] {'"' });
+                            smsObj.Date = HumanReadable(newData);
+                        }
+                        if (attr_Val_array[0].Trim().ToLower() == "address") smsObj.address = attr_Val_array[1];
+
+                        if (attr_Val_array[0].Trim().ToLower() == "type") {
+                            string newType = attr_Val_array[1].TrimEnd(new char[] {'"'});
+                            newType = newType.TrimStart(new char[] {'"' });
+
+                            if (newType == "1") smsObj.Sent_Recieved = "Received";
+                            else if (newType == "2") smsObj.Sent_Recieved = "Sent";
+                        }
+
+                        if (attr_Val_array[0].Trim().ToLower() == "body") smsObj.Message = attr_Val_array[1];
+                        if (attr_Val_array[0].Trim().ToLower() == "read") smsObj.openStatus = attr_Val_array[1];
+                        if (attr_Val_array[0].Trim().ToLower() == "service_center") smsObj.ServiceCenterNumber = attr_Val_array[1];
+                        if (attr_Val_array[0].Trim().ToLower() == "name") smsObj.contactName = attr_Val_array[1];
+
+                        OpenQuoteSeen = false;
+                        CloseQuoteseen = false;
+                        quoteCounter = 0;
+                        dataString = "";
+                    }
+
+
+
+                }
+                else {
+                    dataString = dataString + rawSMS_string[j].ToString();
+                }
+            }
+            
+            return smsObj;
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            FileStream sStream = new FileStream("SMS_OUT.txt", FileMode.CreateNew);
+            StreamWriter smsWriter = new StreamWriter(sStream);
+
+            smsWriter.Write(tbxParsePrev.Text);
+
+
+            smsWriter.Close();
+
+            MessageBox.Show("dONE!!");
+
+            
+        }
+    }
+}
